d5d0c07048c474d2e0ffc48c8060e8c37d74b0f4
Razzile <comfycallum@gmail.com>
2017-02-27 11:14:05 +0000

    Add Circle and Oval parsing code

--- src/ReadShapeFile.java
+++ src/ReadShapeFile.java
@@ -14,7 +14,62 @@ public class ReadShapeFile {
 	
 	//TODO:  You will likely need to write four methods here.  One method to construct each shape
 	//given the Scanner passed as a parameter.  I would suggest four static methods in this case.
-	
+
+	/**
+	 * Construct a new circle object from a scanner input
+	 * @param in the scanner of the file
+	 * @return newly constructed circle
+	 */
+	private static Circle constructCircle(Scanner in) {
+		// position and velocity
+		int px = in.nextInt();
+		int py = in.nextInt();
+		int vx = in.nextInt();
+		int vy = in.nextInt();
+		// should be filled
+		boolean filled = in.nextBoolean();
+		// diameter
+		int diameter = in.nextInt();
+		// red, green and blue colour values
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		// insertion time
+		int insertionTime = in.nextInt();
+
+		Color clr = new Color(r, g, b);
+
+		return new Circle(insertionTime, px, py, vx, vy, diameter, clr, filled);
+	}
+
+	/**
+	 * Construct a new oval object from a scanner input
+	 * @param in the scanner of the file
+	 * @return newly constructed oval
+	 */
+	private static Oval constructOval(Scanner in) {
+		// position and velocity
+		int px = in.nextInt();
+		int py = in.nextInt();
+		int vx = in.nextInt();
+		int vy = in.nextInt();
+		// should be filled
+		boolean filled = in.nextBoolean();
+		// width and height
+		int width = in.nextInt();
+		int height = in.nextInt();
+		// red, green and blue colour values
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		// insertion time
+		int insertionTime = in.nextInt();
+
+		Color clr = new Color(r, g, b);
+
+		return new Oval(insertionTime, px, py, vx, vy, width, height, clr, filled);
+	}
+
 	/**
 	 * Reads the data file used by the program and returns the constructed queue
 	 * @param in the scanner of the file
@@ -34,9 +89,13 @@ public class ReadShapeFile {
 	 * @param filename the name of the file
 	 * @return the queue of shapes from the file
 	 */
-	public static Queue readFile (String filename) {
-		//HINT:  You might want to open a file here.
-		Scanner in = null; //TODO: Change this to construct a proper scanner.
+	public static Queue readFile (String filename) throws FileNotFoundException {
+		File file = new File(filename);
+		if (!file.exists()) {
+			throw new FileNotFoundException("input file not found");
+		}
+
+		Scanner in = new Scanner(file);
 		
 		//TODO:  Read the input file specified by "filename" and return a queue containing
 		//All of the shapes specified in this file
